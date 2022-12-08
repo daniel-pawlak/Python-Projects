b489cf69a1f76191791f58c805c58cd744961a84
Daniel <87375453+daniel-pawlak@users.noreply.github.com>
2022-12-08 09:34:26 +0100

    Update monster_de2.py

--- DE/monster_de2.py
+++ DE/monster_de2.py
@@ -22,7 +22,7 @@ def err(type, value, tb):
     print(traceback.print_tb(tb))
 sys.excepthook = err
 
-path = r'C:\Users\tobiasz\Desktop\VS Code\chromedriver.exe'
+path = r'path\chromedriver.exe'
 opts = Options()
 opts.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36")
 driver = webdriver.Chrome(executable_path=path, chrome_options=opts)
