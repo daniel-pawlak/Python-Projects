349202cbe20ed1384fe0a810129e20d033f700d6
Daniel <87375453+daniel-pawlak@users.noreply.github.com>
2022-12-08 09:29:58 +0100

    Update jzeigen.py

--- DE/jzeigen.py
+++ DE/jzeigen.py
@@ -16,7 +16,7 @@ from selenium.webdriver.common.by import By
 
 url = 'https://www.jobanzeigen.de/'
 
-path = r'C:\Users\tobiasz\Desktop\VS Code\chromedriver.exe'
+path = r'path\chromedriver.exe'
 opts = Options()
 opts.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36")
 driver = webdriver.Chrome(executable_path=path, chrome_options=opts)
